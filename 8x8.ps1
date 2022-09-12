45e18907fd08186fa7c93d1cb8a6957114d76cae
AlexClaw <aclawson@gray.com>
2022-09-12 14:50:04 -0400

    gg

---
+++ 8x8.ps1
@@ -0,0 +1,3 @@
+netsh advfirewall firewall add rule name="8x8 Work" dir=in action=allow program="C:\program files\8x8 inc\8x8 work\8x8 work.exe" enable=yes profile=private
+
+netsh advfirewall firewall add rule name="8x8 Work" dir=in action=allow program="C:\program files\8x8 inc\8x8 work\8x8 work.exe" enable=yes profile=public
